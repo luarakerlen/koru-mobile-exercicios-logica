9c4faebfff40f2fa587364a524df34f01a4d4287
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-18 22:17:10 -0300

    chore: solution exercise 1

--- desafio1.dart
+++ desafio1.dart
@@ -1,3 +1,12 @@
 void main() {
-  // Escreva o código aqui...
+  int valorA = 23;
+  int valorB = 23;
+
+  if(valorA > valorB) {
+    print('O valor $valorA é maior que o valor $valorB');
+  } else if(valorB > valorA) {
+    print('O valor $valorB é maior que o valor $valorA');
+  } else {
+    print('O valor $valorA é igual ao valor $valorB');
+  }
 }
