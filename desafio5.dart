f2eb9fa5249f154fb8e5cdfd75e1405fcf6030ac
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 19:36:02 -0300

    chore: solution exercise 5

--- desafio5.dart
+++ desafio5.dart
@@ -1,3 +1,12 @@
+/**
+ * Faça uma função que inicialize um número inteiro qualquer.
+ * Como resultado, imprima seu antecessor e o seu sucessor.
+ */
+
 void main() {
-  // Escreva o código aqui...
+  int numero = 45;
+  int antecessor = numero - 1;
+  int sucessor = numero + 1;
+
+  print('O antecessor de $numero é $antecessor e o sucessor é $sucessor.');
 }
