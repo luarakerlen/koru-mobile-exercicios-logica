7931323f4ce88594c6cd7a8a24c66f0ee8db6525
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 19:52:05 -0300

    chore: solution exercise 6

--- desafio6.dart
+++ desafio6.dart
@@ -1,3 +1,16 @@
+/**
+ * Faça uma função que inicialize o valor do salário mínimo e
+ * o valor do salário de um usuário em duas variáveis diferentes.
+ * Calcule quantos salários mínimos esse usuário ganha e imprima o resultado.
+ * (Base para o salário mínimo R$ 1.412,00).
+ */
+
 void main() {
-  // Escreva o código aqui...
+  const double salarioMinimo = 1412.00;
+  double salario = 10000;
+
+  double salarios = salario / salarioMinimo;
+  String salariosArredondado = salarios.toStringAsFixed(2);
+
+  print('Esse funcionário recebe $salariosArredondado salários mínimos.');
 }
