c289fa46518e182ca9c81c336ef8e6b804d7241c
Daniela <danielasouza0729@gmail.com>
2024-03-22 01:15:17 -0300

    feat/desafio5

--- desafio5.dart
+++ desafio5.dart
@@ -1,3 +1,9 @@
 void main() {
-  // Escreva o código aqui...
+  //Faça uma função que inicialize um número inteiro qualquer. Como resultado, imprima seu antecessor e o seu sucessor.
+
+  int numeroD  = 50;
+  int numeroE = numeroD - 1;
+  int numeroF = numeroD + 1;
+  print('o numero é $numeroD, seu antecessor é $numeroE e seu sucessor é $numeroF');
+  
 }
