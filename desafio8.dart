67bd4b15bbd3afcea69ba130cd0c892c1e11b237
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 20:33:21 -0300

    chore: solution exercise 8

--- desafio8.dart
+++ desafio8.dart
@@ -1,3 +1,21 @@
+/**
+ * Crie uma função que inicialize uma lista de notas de um aluno, calcule a média das notas.
+ * Como resultado, imprima a média do aluno e se ele foi aprovado ou reprovado (média para aprovação: 7).
+ */
+
 void main() {
-  // Escreva o código aqui...
+  List<double> notas = [5, 10, 7, 6];
+  double soma = 0;
+  
+  // Explicação de como o for funciona:
+  // for(inicializa um contador; condição de parada; incremento)
+  // for(var i = 0; i < 10; i++) {
+  //   print(i);
+  // }
+
+  // Como acessar posição específica
+  // print(notas[0]);
+
+  // Como saber o tamanho da lista
+  // print(notas.length);
 }
