6b09f4b9841445baf4b9f14e664b60e4e8984014
Daniela <danielasouza0729@gmail.com>
2024-03-22 00:58:49 -0300

    feat/desafio4

--- desafio4.dart
+++ desafio4.dart
@@ -1,3 +1,15 @@
 void main() {
-  // Escreva o código aqui...
+  // Faça uma função que inicialize dois valores inteiros A e B. Se os valores de A e B forem iguais, deverá somar os dois valores, caso contrário deverá multiplicar A por B. Ao final de qualquer um dos cálculos deve-se imprimir o resultado.
+
+   int valorA = 30;
+  int valorB = 10;
+  
+  
+  
+  
+  if(valorA == valorB){
+    print(valorA + valorB);
+  }else{
+    print(valorA * valorB);
+  }
 }
