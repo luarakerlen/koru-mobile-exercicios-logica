bcadf353d75a8a6f95b62906e951ab62d375b53f
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 19:32:35 -0300

    chore: solution exercise 4

--- desafio4.dart
+++ desafio4.dart
@@ -1,3 +1,19 @@
+/**
+ * Faça uma função que inicialize dois valores inteiros A e B.
+ * Se os valores de A e B forem iguais, deverá somar os dois valores,
+ * caso contrário deverá multiplicar A por B.
+ * Ao final de qualquer um dos cálculos deve-se imprimir o resultado.
+ */
+
 void main() {
-  // Escreva o código aqui...
+  int valorA = 24;
+  int valorB = 24;
+
+  if(valorA == valorB) {
+    int soma = valorA + valorB;
+    print('A soma entre $valorA e $valorB é $soma');
+  } else {
+    int produto = valorA * valorB;
+    print('O produto entre $valorA e $valorB é $produto');
+  }
 }
