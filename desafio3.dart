17acd9a4950fd07c4442b9e3ff4f5d463d62d1d1
Daniela <danielasouza0729@gmail.com>
2024-03-22 00:35:01 -0300

    desafio3

--- desafio3.dart
+++ desafio3.dart
@@ -1,3 +1,19 @@
 void main() {
-  // Escreva o código aqui...
+  // Crie uma função que inicialize um número qualquer. Como resultado, imprima se o número é par ou ímpar e positivo ou negativo.
+  int numero = 20;
+  
+  
+  
+  
+  if(numero%2==0){
+    print("$numero é par");
+  }else{
+    print("$numero é impar");
+  }
+  
+  if(numero<0){
+    print("$numero é negativo");
+  }else{
+    print("$numero é positivo");
+  }
 }
