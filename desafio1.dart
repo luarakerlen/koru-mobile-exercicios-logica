76a43ecb595af0f9d5a2dd23820bdb6ed5168e05
Daniela <danielasouza0729@gmail.com>
2024-03-21 22:20:07 -0300

    feat/desafio1

--- desafio1.dart
+++ desafio1.dart
@@ -1,3 +1,11 @@
 void main() {
   // Escreva o código aqui...
+  int valorA = 5;
+  int valorB = 10;
+  
+  if(valorA > valorB){
+    print("o $valorA é maior que o $valorB");
+  }else(valorB > valorA); {
+    print("o $valorB  é maior que o $valorA");
+  };
 }
