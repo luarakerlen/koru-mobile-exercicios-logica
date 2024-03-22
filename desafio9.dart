bec75632187be23a34b8f212b1ab2f01b3355507
Daniela <danielasouza0729@gmail.com>
2024-03-22 14:43:39 -0300

    desafio9

--- desafio9.dart
+++ desafio9.dart
@@ -1,3 +1,12 @@
 void main() {
   // Escreva o código aqui...
+  
+  String nome = ('daniela');
+  int idade = 27;
+  print("$nome");
+  if(idade >= 18){
+    print("$nome é maior de idade");
+  }else{
+    print("$nome é menor de idade");
+  }
 }
