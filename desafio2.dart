361ada5ce8f3813bb2d61f39f70c9b659efb804d
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 18:56:12 -0300

    chore: solution exercise 2

--- desafio2.dart
+++ desafio2.dart
@@ -1,3 +1,20 @@
+/** Crie uma função que inicialize os valores de A, B, C.
+ * Como resultado, imprima a soma entre A e B e se a soma é maior ou menor do que C.
+ */
+
 void main() {
-  // Escreva o código aqui...
+  int valorA = 2;
+  int valorB = 13;
+  int valorC = 10;
+
+  int soma = valorA + valorB;
+  print('A soma entre $valorA e $valorB é igual a $soma');
+
+  if(soma > valorC) {
+    print('A soma é maior do que $valorC');
+  } else if(soma < valorC) {
+    print('A soma é menor do que $valorC');
+  } else {
+    print('A soma é igual ao valor de C - $valorC');
+  }
 }
