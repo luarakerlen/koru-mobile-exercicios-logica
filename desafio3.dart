9c9f4f537fefeb5ab839b7c737768f1abf378045
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 19:19:25 -0300

    chore: solution exercise 3

--- desafio3.dart
+++ desafio3.dart
@@ -1,3 +1,23 @@
+/**
+ * Crie uma função que inicialize um número qualquer.
+ * Como resultado, imprima se o número é par ou ímpar e positivo ou negativo.
+ */
+
 void main() {
-  // Escreva o código aqui...
+  int numero = -85;
+  int resto = numero % 2;
+
+  // Par ou ímpar
+  if (resto == 0) {
+    print('O número $numero é par');
+  } else {
+    print('O número $numero é ímpar');
+  }
+
+  // Positivo ou negativo
+  if (numero >= 0) {
+    print('O número $numero é positivo');
+  } else {
+    print('O número $numero é negativo');
+  }
 }
