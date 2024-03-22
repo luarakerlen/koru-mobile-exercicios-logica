c2aaa8b4a739e14ccd48e11a7652fd776f766a35
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-22 19:56:24 -0300

    chore: solution exercise 7

--- desafio7.dart
+++ desafio7.dart
@@ -1,3 +1,13 @@
+/**
+ * Crie uma função que inicialize três valores inteiros diferentes.
+ * Como resultado, imprima os valores em ordem crescente.
+ * Exemplo: entrada = [5, 1, 9] | saída = [1, 5, 9]
+ */
+
 void main() {
-  // Escreva o código aqui...
+  List<int> numeros = [5, 1, 9, 7, 23, 56, 1];
+
+  numeros.sort();
+
+  print(numeros);
 }
