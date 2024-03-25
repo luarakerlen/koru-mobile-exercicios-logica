21f8fc6419e661438fdd42ffe82c911c27108189
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-25 16:51:55 -0300

    chore: beginning of exercise 8 solution

--- desafio8.dart
+++ desafio8.dart
@@ -3,19 +3,19 @@
  * Como resultado, imprima a média do aluno e se ele foi aprovado ou reprovado (média para aprovação: 7).
  */
 
+// Explicação de como o for funciona:
+// for(inicializa um contador; condição de parada; incremento)
+// for(var i = 0; i < 10; i++) {
+//   print(i);
+// }
+
+// Como acessar posição específica
+// print(notas[0]);
+
+// Como saber o tamanho da lista
+// print(notas.length);
+
 void main() {
   List<double> notas = [5, 10, 7, 6];
   double soma = 0;
-  
-  // Explicação de como o for funciona:
-  // for(inicializa um contador; condição de parada; incremento)
-  // for(var i = 0; i < 10; i++) {
-  //   print(i);
-  // }
-
-  // Como acessar posição específica
-  // print(notas[0]);
-
-  // Como saber o tamanho da lista
-  // print(notas.length);
 }
