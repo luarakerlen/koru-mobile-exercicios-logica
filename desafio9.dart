866c858788744b07b6098561ec428332b72ad29b
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-25 22:07:40 -0300

    chore: solution exercise 9

--- desafio9.dart
+++ desafio9.dart
@@ -1,3 +1,15 @@
+/**
+ * Crie uma função que inicialize o nome e a idade de uma pessoa.
+ * Como resultado, imprima o nome da pessoa e se ela é maior ou menor de idade.
+ */
+
 void main() {
-  // Escreva o código aqui...
+  String nome = 'Maria';
+  int idade = 13;
+
+  if(idade >= 18) {
+    print('$nome é maior de idade.');
+  } else {
+    print('$nome é menor de idade');
+  }
 }
