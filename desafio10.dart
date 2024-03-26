766d33ab47c46d0ae2b1aeeeafe4bd6a7d8d06be
Luara Rangel <26902816+luarakerlen@users.noreply.github.com>
2024-03-25 22:27:33 -0300

    chore: solution exercise 10

--- desafio10.dart
+++ desafio10.dart
@@ -1,3 +1,19 @@
+/**
+ * Crie uma função que inicialize um número inteiro e
+ * imprima a tabuada de multiplicação de 1 a 10 desse número.
+ */
+
 void main() {
-  // Escreva o código aqui...
+  int numero = 5;
+
+  // 1 x 5 = 5;
+  // 2 x 5 = 10;
+  // 3 x 5 = 15;
+  // ...
+  // 10 x 5 = 50;
+
+  for(var i = 1; i <= 10; i++) {
+    int produto = numero * i;
+    print('$i x $numero = $produto');
+  }
 }
